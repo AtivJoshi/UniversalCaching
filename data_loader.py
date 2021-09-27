495394e96960fc2f37a259e6a59ffba48f80decd
AtivJoshi <ativsc@gmail.com>
2021-09-27 22:19:36 +0530

    added folder path option in data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -2,10 +2,10 @@ import numpy as np
 import pandas as pd
 import os.path
 
-def load_ratings_data(num_users:int, num_files:int)->np.ndarray:
+def load_ratings_data(num_users:int, num_files:int,folder_path:str="")->np.ndarray:
     file_name='ratings'
-    file_path=f'data/{file_name}.dat'
-    cache_path=f'data/{file_name}_{num_users}u_{num_files}f_cache.npy'
+    file_path=folder_path+f'data/{file_name}.dat'
+    cache_path=folder_path+f'data/{file_name}_{num_users}u_{num_files}f_cache.npy'
 
     if os.path.isfile(cache_path):
         return np.load(cache_path)
@@ -29,10 +29,10 @@ def load_ratings_data(num_users:int, num_files:int)->np.ndarray:
         np.save(cache_path,input_seq)
         return input_seq
 
-def load_cmu_data(num_users:int, num_files:int)->np.ndarray:
+def load_cmu_data(num_users:int, num_files:int,folder_path:str="")->np.ndarray:
     file_name="CMU_huge"
-    file_path=f'data/{file_name}.txt'
-    cache_path=f'data/{file_name}_{num_users}u_{num_files}f_cache.npy'
+    file_path=folder_path+f'data/{file_name}.txt'
+    cache_path=folder_path+f'data/{file_name}_{num_users}u_{num_files}f_cache.npy'
 
     if os.path.isfile(cache_path):
         return np.load(cache_path)
@@ -53,9 +53,9 @@ def load_cmu_data(num_users:int, num_files:int)->np.ndarray:
         np.save(cache_path,input_seq)
         return input_seq        
 
-def load_synthetic_data(num_users:int, num_files:int, user:int=0)->np.ndarray:
-    file_name=f'synthetic_user_{user}.npy'
-    file_path=f'data/{file_name}'
+def load_synthetic_data(num_users:int, num_files:int, idx:int=0, folder_path:str="")->np.ndarray:
+    file_name=f'synthetic_user_{idx}.npy'
+    file_path=folder_path+f'data/{file_name}'
 
     raw_seq=np.load(file_path)
     # split raw_seq into chunks of size <num_users>

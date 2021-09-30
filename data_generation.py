52167de1ebcfaa902cf19a95560874bc5e47ea91
AtivJoshi <ativsc@gmail.com>
2021-09-30 18:52:03 +0530

    data is now generated using a markov chaiin with a random erdos renyi graph

--- data_generation.py
+++ data_generation.py
@@ -1,5 +1,6 @@
 import numpy as np
-from numba import njit
+from tqdm import tqdm
+import networkx as nx
 
 """
 Generate synthetic data for IPLC using markov chain. 
@@ -7,23 +8,33 @@ For each user, initialize a random N x N transition matrix, where N is the numbe
 """
 def generate_data(num_files:int, total_time:int)->np.ndarray:
     transitionMatrix:np.ndarray = np.random.rand(num_files,num_files) # generate random matrix
+    mask=random_graph(num_files)
+    transitionMatrix=transitionMatrix*mask
     transitionMatrix = transitionMatrix/transitionMatrix.sum(axis=1,keepdims=True) # normalize the rows
     state:int=np.random.randint(num_files)
     seq:np.ndarray=np.zeros(total_time)
-    for i in range(total_time):
+    for i in tqdm(range(total_time)):
         state=np.random.choice(num_files,p=transitionMatrix[state,:])
         seq[i]=state
-        if i%10000==0:
-            print(i/100,end=" ",flush=True)
     return seq
+
+# generates uniformly random (not really!) connected graph 
+def random_graph(nodes:int=100,p:float=0.05)->np.ndarray:
+    G=nx.gnp_random_graph(nodes,0.05)
+    while(not nx.is_connected(G)):
+        G=nx.gnp_random_graph(nodes,0.05)
+
+    A=nx.adjacency_matrix(G)
+    # A.to_numpy_array()
+    return np.array(A.todense())
 
 def main():
     N=100
     T=1000000
-    users=10
+    users=1
     for user in range(users):
         seq=generate_data(N,T)
-        with open('data/synthetic_user_'+str(user)+'.npy','wb') as f:
+        with open('data/synthetic_erdos_renyi_'+str(user+1)+'.npy','wb') as f:
             np.save(f,seq)
 
 if __name__=="__main__":

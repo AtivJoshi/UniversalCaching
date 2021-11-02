98f5b0061877e59b741ccdb4e22becacd3ee9c35
AtivJoshi <ativsc@gmail.com>
2021-11-02 15:10:46 +0530

    added offlin markov and new ratings results

--- caching_policy.py
+++ caching_policy.py
@@ -173,101 +173,71 @@ def iplc_multiple_fsm(
     return states_visits, states_hits, {}
 
 
-# def markov_offline(
-#         input_seq:np.ndarray, 
-#         adj_mat:np.ndarray,
-#         total_time:int,
-#         num_files:int,
-#         cache_size:int,
-#         deg:int
-#     )->Tuple[Dict[Tuple[int,str],int],Dict[Tuple[int,str],int]]:
-
-#     num_users, num_cache=np.shape(adj_mat)
-#     total_time=min(input_seq.shape[0],total_time)
-
-
-################################################################################################################################################
-
-
-# DO NOT USE
-def _iplc(
+def markov_offline(
         input_seq:np.ndarray, 
         adj_mat:np.ndarray,
         total_time:int,
         num_files:int,
         cache_size:int,
-        deg:int
-    )->Tuple[Dict[str,int],Dict[str,int]]:
-    """Incremental Parsing with Lead Cache
-        ATTENTIOIN: DO NOT USE iplc
-        USE iplc_multiple_fsm
+        deg:int,
+        k:int
+    ):
 
-        Arguments:
-        input_seq: input sequence of file requests
-        adj_mat: adjacency matrix of the bipartite graph
-        total_time: algorithm will run for min(total_time, len(input_seq))
-        num_files: library size
-        cache_size: storage space of each cache machine
-        deg: degree of each cache machine
-
-        Outputs:
-        states_visits: total number of time the state is visited
-        states_hits: total number of hits at the state
-    """
+    '''
+        k: order of markov fsm
+    '''
 
     num_users, num_cache=np.shape(adj_mat)
     total_time=min(input_seq.shape[0],total_time)
-    # constants as defined in lead cache algorithm
-    gamma:np.ndarray=np.random.normal(0,1,(num_users,num_files))
-    eta_constant:float = math.pow(num_users, 0.75)/(math.pow(2*deg*(math.log(num_files/cache_size) + 1), 0.25)*(math.pow(2*num_cache*cache_size, 0.5)))
-    constr_violation_tol = 1.0
+    markov:Dict[Tuple[int,Tuple],np.ndarray]={}
+    # current state of a given user
+    current_state:Dict[int,Tuple]={}
 
-    # A state is labeled by the parse sequence of LZ algorithm, where the initial 
-    # state is labeled 'epsilon'.
-    # State corresponding to sequence (34,45,90) is labeled by 'epsilon:34:45:90'
-    init_state='epsilon'
+    # no of times a state is visited for a particular user
+    states_visits:Dict[Tuple[int,Tuple],int]={}
 
-    # dict that stores cumulative request for each state
-    states_cumulative_request={init_state:np.zeros((num_users,num_files))}
+    # no of hits at a given state of a user
+    states_hits:Dict[Tuple[int,Tuple],int]={}
 
-    # dict that store number of hits for each state
-    states_hits={init_state:0}
-    states_visits={init_state:0}
-    current_state=init_state
+    for i in range(num_users):
+        markov[(i,tuple(input_seq[:k,i]))]=np.zeros(num_files) # index is (user,current_state)
+        current_state[i]=tuple(input_seq[:k,i]) # index is user
+        states_hits[(i,tuple(input_seq[:k,i]))]=0 # index is (user,current_state)
+        states_visits[(i,tuple(input_seq[:k,i]))]=0 # index is (user,current_state)
 
-    for t in tqdm(range(total_time)):
-        Xr:np.ndarray=states_cumulative_request[current_state]
-        theta = np.maximum(Xr + eta_constant*(math.pow((t+1),0.5))*gamma, 0) # taking the non-negative part of theta only
+    # construct markov fsm
+    for t in range(3,total_time):
+        for u in range(num_users):
+            states_visits[(u,current_state[u])]+=1
+            m:int=input_seq[t,u]
+            markov[(u,current_state[u])][m]+=1
+            next_state=current_state[u][1:]+(m,)
+            if (u,next_state) not in markov:
+                markov[(u,next_state)]=np.zeros(num_files)
+                states_hits[(u,next_state)]=0
+                states_visits[(u,next_state)]=0
+            current_state[u]=next_state
 
-        # y_t: cache configuration predicted at time t
-        y_t,_ = SolveLP(adj_mat, theta, cache_size, t)
-        y_madow = np.rint(madow_rounding(y_t, theta, adj_mat, cache_size))
+    # for single cache, the optimal policy is to fetch most often requested files
+    # for multiple caches, do we approximate using relaxed LP + madow's sampling?
+    for i in range(num_users):
+        current_state[i]=tuple(input_seq[:k,i])
 
-        # update the cumulative request and total visit count of current_state
-        states_visits[current_state]+=1
-        for i in range (num_users):
-            m=input_seq[t,i]
-            Xr[i,m]+=1
-
-        # update the total hit count of current state
-        num_hits=0
-        for user in range(num_users):
-            for f in np.flatnonzero(adj_mat[user][:]):
-                if y_madow[f][int(input_seq[t,user])] >0.998:
-                    num_hits+=1
-                    break # since each user requests exactly one file
-        states_hits[current_state]+=num_hits
-
-        # go to next state
-        next_state=f'{current_state}:{np.array2string(input_seq[t,:])}'
-
-        # if the next_state is encountered for the first time, then initialize it and go to init_state, else go to next_state
-        if next_state not in states_cumulative_request:
-            states_cumulative_request[next_state]=np.zeros((num_users,num_files))
-            states_hits[next_state]=0
-            states_visits[next_state]=0
-            current_state=init_state
-        else:
-            current_state=next_state
-    return states_visits, states_hits
-
+    for t in tqdm(range(k,total_time)):
+        Xr=np.zeros((num_users,num_files))
+        for i in range(num_users):
+            # print(f'{t} {i} {current_state[i]}')
+            Xr[i,:]=markov[(i,current_state[i])]
+        y_t,_ = SolveLP(adj_mat, Xr, cache_size, t)
+        y_madow = np.rint(madow_rounding(y_t, Xr, adj_mat, int(cache_size)))
+        for u in range(num_users):
+            for f in np.flatnonzero(adj_mat[u][:]):
+                if y_madow[f][int(input_seq[t,u])] >0.998:
+                    states_hits[(u,current_state[u])]+=1
+                    break # since a user requests exactly one file at a time
+        for u in range(num_users):
+            m:int=input_seq[t,u]
+            next_state=current_state[u][1:]+(m,)
+            current_state[u]=next_state
+        
+    return states_visits,states_hits
